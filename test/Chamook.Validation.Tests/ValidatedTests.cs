56f4601ca0d30d728f3aaac28099334699d2a10e
Adam Guest <adam@chamook.lol>
2024-10-06 15:25:45 +0200

    Add tests for And

--- test/Chamook.Validation.Tests/ValidatedTests.cs
+++ test/Chamook.Validation.Tests/ValidatedTests.cs
@@ -29,4 +29,63 @@ public sealed class ValidatedTests
                 ifError: e => e);
         Assert.Equal(input, result);
     }
+
+    [Fact]
+    public void AndReturnsBothValidValues()
+    {
+        var first = Guid.NewGuid();
+        var second = Guid.NewGuid();
+
+        var result =
+            Validated<Guid, string>.Valid(first)
+            .And(Validated<Guid, string>.Valid(second));
+
+        Assert.True(result.IsValid);
+        Assert.Equal(first, result.GetValidValueOrThrow().Item1);
+        Assert.Equal(second, result.GetValidValueOrThrow().Item2);
+    }
+
+    [Fact]
+    public void AndReturnsErrorForFirstFailure()
+    {
+        var error = Guid.NewGuid().ToString();
+
+        var result =
+            Validated<Guid, string>.Error(error)
+            .And(Validated<Guid, string>.Valid(Guid.NewGuid()));
+
+        Assert.False(result.IsValid);
+        var returnedError = Assert.Single(result.GetErrorOrThrow());
+        Assert.Equal(error, returnedError);
+    }
+
+    [Fact]
+    public void AndReturnsErrorForSecondFailure()
+    {
+        var error = Guid.NewGuid().ToString();
+
+        var result =
+            Validated<Guid, string>.Valid(Guid.NewGuid())
+            .And(Validated<Guid, string>.Error(error));
+
+        Assert.False(result.IsValid);
+        var returnedError = Assert.Single(result.GetErrorOrThrow());
+        Assert.Equal(error, returnedError);
+    }
+
+    [Fact]
+    public void AndReturnsBothErrors()
+    {
+        var error1 = Guid.NewGuid().ToString();
+        var error2 = Guid.NewGuid().ToString();
+
+        var result =
+            Validated<Guid, string>.Error(error1)
+            .And(Validated<Guid, string>.Error(error2));
+
+        Assert.False(result.IsValid);
+        var errors = result.GetErrorOrThrow();
+        Assert.Contains(error1, errors);
+        Assert.Contains(error2, errors);
+    }
 }
